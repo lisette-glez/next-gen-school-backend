9a8dda228cec01ab67b69104cb398f6a95d6a3dd
Lisette Gonzalez <contact@lisette.dev>
2024-08-16 21:06:01 -0400

    feature: add module model

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -5,17 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Module extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
-    
-    /**
-     * Get the modules for the course.
-     */
-    public function modules()
-    {
-        return $this->hasMany(Module::class);
-    }
 }
---
+++ database/factories/ModuleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Course;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Module>
+ */
+class ModuleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'course_id' => Course::factory(),
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'order' => fake()->randomDigit()
+        ];
+    }
+}
