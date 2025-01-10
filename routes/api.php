bab6db6cc4c7cd0b6c3246dc77cb24272a76194d
Lisette Gonzalez <contact@lisette.dev>
2025-01-10 17:45:59 -0500

    feature: added moduleController and route for retrieving modules

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ModuleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/course/{id}', [CourseController::class, 'show']);
+
+Route::get('/modules', [ModuleController::class, 'index']);
